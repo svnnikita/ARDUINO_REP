efc06a6bcb4d941b61e329f969fb62ddd26e3449
svnnikita <svnnikitos@gmail.com>
2024-10-23 18:21:47 +0300

    Шаблоны, игра со светодиодом

--- src/led_templ.h
+++ src/led_templ.h
@@ -7,6 +7,50 @@ class LED {
     static void off() {digitalWrite(pin, LOW);}
 };
 
+template<int pin, int rpin, int gpin, int bpin>
+class RGB_LED {
+    public:
+    RGB_LED() {};
+
+    static void init() {pinMode(pin, OUTPUT); pinMode(rpin, OUTPUT); pinMode(gpin, OUTPUT); pinMode(bpin, OUTPUT);}
+
+    static void on() {digitalWrite(pin, HIGH);}
+
+    static void redLightOn() {digitalWrite(rpin, LOW); digitalWrite(gpin, HIGH); digitalWrite(bpin, HIGH);}
+
+    static void off() {digitalWrite(pin, LOW);}
+
+    static void redLightOff() {digitalWrite(rpin, LOW); digitalWrite(gpin, LOW); digitalWrite(bpin, LOW);}
+
+    static void clrRGB(char color) {
+        switch (color) {
+            case 'r':
+                digitalWrite(rpin, LOW);
+                digitalWrite(gpin, HIGH);
+                digitalWrite(bpin, HIGH);
+                break;
+            case 'g':
+                digitalWrite(rpin, HIGH);
+                digitalWrite(gpin, LOW);
+                digitalWrite(bpin, HIGH);
+                break;
+            case 'b':
+                digitalWrite(rpin, HIGH);
+                digitalWrite(gpin, HIGH);
+                digitalWrite(bpin, LOW);
+                break;
+            case 'w':
+                digitalWrite(rpin, LOW);
+                digitalWrite(gpin, LOW);
+                digitalWrite(bpin, LOW);
+                break;
+            default:
+                Serial.print('?');
+                break;
+        }
+    }
+};
+
 void delay_s(unsigned int s) {
     delay(s * 1000);
 }
